07f703fb5373aa65a3c60c315d1fbf4f33654ebd
Karilyn-ACE <karilynuwu@gmail.com>
2025-05-13 12:58:18 -0600

    Cambios en el servidor

--- Servidor.java
+++ Servidor.java
@@ -4,17 +4,35 @@ import java.io.*;
 public class Servidor {
    public static void main(String argv[]) throws Exception  
    {  
-          String clientSentence;        
-          String capitalizedSentence;      
+          String peticionCliente;        
+          String resultado;      
              ServerSocket welcomeSocket = new ServerSocket(6789);    
                     while(true) {                
                          Socket connectionSocket = welcomeSocket.accept();            
                            BufferedReader inFromClient =  new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));  
     DataOutputStream  outToClient =  new DataOutputStream(connectionSocket.getOutputStream());  
-    clientSentence = inFromClient.readLine(); 
-    
 
-    capitalizedSentence = clientSentence.toUpperCase() + '\n';  
-    
-    outToClient.writeBytes(capitalizedSentence);  
-   }}}
+   peticionCliente = inFromClient.readLine(); 
+
+        String[]p =peticionCliente.split(":",2);
+            String codigo = p[0];
+            double valor = Double.parseDouble(p[1]);    
+            
+            double resultadoCliente;
+            if ("F2C".equals(codigo)){
+                resultadoCliente = (valor - 32)* 5/  9;
+               
+                resultado= "C:"
+        + String.format("%.4f",resultadoCliente) + "\n";  }
+
+        else{
+           resultadoCliente = valor * 9/5+32;
+           resultado = "F:" + String.format("%.4f",resultadoCliente) + "\n";  
+        }
+        outToClient.writeBytes(resultado);
+        System.out.println("Petición: "+ peticionCliente + "respuesta: " + resultadoCliente);
+
+
+        }}}
+
+            
